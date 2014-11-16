3ba09043cae3ccc2678dd699d6ae164eb369cec7
Hideki Tsukamoto <dortmund23andcska18@gmail.com>
2014-11-16 12:59:11 +0900

    Initial Commit

---
+++ EarClerics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarClerics
+//
+//  Created by hideki on 2014/11/16.
+//  Copyright (c) 2014年 Tsubaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
